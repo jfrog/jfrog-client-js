9684c8a1cab567f653579abaf7a1703486bddafa
Yahav Itzhak <yahavi@users.noreply.github.com>
2020-02-23 11:30:01 +0200

    Upgrade dependencies (#3)

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ export { HttpClient } from './HttpClient';
 export { SummaryClient } from './SummaryClient';
 export { SystemClient } from './SystemClient';
 export { XrayClient } from './XrayClient';
-
