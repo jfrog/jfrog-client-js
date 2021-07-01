ea1b98d6a3aa536f4ae6c0a92eda1820c29e78d8
Yahav Itzhak <yahavi@users.noreply.github.com>
2021-07-01 20:09:24 +0300

    Add missing sha256 and parent_sha256 to IGeneral (#25)

--- model/Xray/Summary/General.ts
+++ model/Xray/Summary/General.ts
@@ -1,7 +1,9 @@
 export interface IGeneral {
     name: string;
     path: string;
+    sha1: string;
     sha256: string;
     pkg_type: string;
+    parent_sha256: string[];
     component_id: string;
 }
