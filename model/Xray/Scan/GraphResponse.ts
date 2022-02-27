478f44bef7b960bef755120de2f7e9b4b8e93e6f
yahavi <yahavi@jfrog.com>
2022-02-27 17:42:28 +0200

    Add progress for /scan/graph API

--- model/Xray/Scan/GraphResponse.ts
+++ model/Xray/Scan/GraphResponse.ts
@@ -5,6 +5,7 @@ import { IVulnerability } from './Vulnerability';
 export interface IGraphResponse {
     scan_id: string;
     package_type: string;
+    progress_percentage: number;
     licenses: IGraphLicense[];
     violations: IViolation[];
     vulnerabilities: IVulnerability[];
--- src/Xray/XrayScanClient.ts
+++ src/Xray/XrayScanClient.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '..';
 import { IGraphRequestModel, IGraphResponse, ILogger } from '../../model/';
 import { IRequestParams } from '../HttpClient';
+import { XrayScanProgress } from './XrayScanProgress';
 
 export class XrayScanClient {
     private static readonly scanGraphEndpoint: string = 'api/v1/scan/graph';
@@ -11,29 +12,41 @@ export class XrayScanClient {
 
     public async graph(
         model: IGraphRequestModel,
+        progress: XrayScanProgress,
         checkCanceled: () => void,
         projectKey: string | undefined,
         sleepIntervalMilliseconds: number = XrayScanClient.SLEEP_INTERVAL_MILLISECONDS
     ): Promise<IGraphResponse> {
-        if (!model) {
-            return {} as IGraphResponse;
-        }
+        try {
+            if (!model) {
+                return {} as IGraphResponse;
+            }
 
-        const projectProvided: boolean = projectKey !== undefined && projectKey.length > 0;
-        this.logger.debug('Sending POST scan/graph request...');
-        const requestParams: IRequestParams = {
-            url: XrayScanClient.scanGraphEndpoint + (projectProvided ? `?project=${projectKey}` : ''),
-            method: 'POST',
-            data: model,
-        };
-        this.logger.debug('data: ' + JSON.stringify(model));
-        checkCanceled();
-        const response: IGraphResponse = await this.httpClient.doAuthRequest(requestParams);
-        return this.getScanGraphResults(response.scan_id, checkCanceled, !projectProvided, sleepIntervalMilliseconds);
+            const projectProvided: boolean = projectKey !== undefined && projectKey.length > 0;
+            this.logger.debug('Sending POST scan/graph request...');
+            const requestParams: IRequestParams = {
+                url: XrayScanClient.scanGraphEndpoint + (projectProvided ? `?project=${projectKey}` : ''),
+                method: 'POST',
+                data: model,
+            };
+            this.logger.debug('data: ' + JSON.stringify(model));
+            checkCanceled();
+            const response: IGraphResponse = await this.httpClient.doAuthRequest(requestParams);
+            return await this.getScanGraphResults(
+                response.scan_id,
+                progress,
+                checkCanceled,
+                !projectProvided,
+                sleepIntervalMilliseconds
+            );
+        } finally {
+            progress.setPercentage(100);
+        }
     }
 
     private async getScanGraphResults(
         scanId: string,
+        progress: XrayScanProgress,
         checkCanceled: () => void,
         includeVulnerabilities: boolean,
         sleepIntervalMilliseconds: number
@@ -47,14 +60,15 @@ export class XrayScanClient {
         for (let i: number = 0; i < XrayScanClient.MAX_ATTEMPTS; i++) {
             checkCanceled();
             this.logger.debug(`Sending GET ${scanGraphUrl} request...`);
+            let receivedStatus: number | undefined;
             const requestParams: IRequestParams = {
                 url: scanGraphUrl,
                 method: 'GET',
                 validateStatus: (status: number): boolean => {
-                    return status === 200;
+                    receivedStatus = status;
+                    return status === 200 || status === 202;
                 },
             };
-            let receivedStatus: number | undefined;
             let message: string | undefined;
             const response: IGraphResponse = await this.httpClient
                 .doAuthRequest(requestParams)
@@ -62,13 +76,23 @@ export class XrayScanClient {
                     receivedStatus = reason.response?.status;
                     message = reason?.message;
                 });
-            if (receivedStatus === 202) {
-                await this.delay(sleepIntervalMilliseconds);
-            } else if (receivedStatus) {
-                throw new Error(`Received unexpected status '${receivedStatus}' from Xray: ${message}`);
-            } else {
+            this.logger.debug(`Received status '${receivedStatus}' from Xray.`);
+            if (receivedStatus === 200) {
                 return response;
             }
+
+            if (receivedStatus === 202) {
+                if (response.progress_percentage) {
+                    progress.setPercentage(response.progress_percentage);
+                }
+            } else {
+                if (receivedStatus) {
+                    throw new Error(`Received unexpected status '${receivedStatus}' from Xray: ${message}`);
+                }
+                throw new Error(`Received response from Xray: ${message}`);
+            }
+
+            await this.delay(sleepIntervalMilliseconds);
         }
         throw new Error('Xray get scan graph exceeded the timeout.');
     }
