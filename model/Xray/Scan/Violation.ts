8a1064ebc7e0f01f06fac03f0665d771fdcf71c7
Assaf Attias <49212512+attiasas@users.noreply.github.com>
2022-12-06 10:23:47 +0200

    adding attributes in graph response (#62)

--- model/Xray/Scan/Violation.ts
+++ model/Xray/Scan/Violation.ts
@@ -1,3 +1,5 @@
 import { IGraphLicense, IVulnerability } from '.';
 
-export interface IViolation extends IVulnerability, IGraphLicense {}
+export interface IViolation extends IVulnerability, IGraphLicense {
+    watch_name: string;
+}
