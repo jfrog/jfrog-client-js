d465b9ba1cef3c7d491dc12ef9413d4042e096c8
attiasas <assafattias93@gmail.com>
2022-12-06 10:14:08 +0200

    adding attributes in graph response

--- model/Xray/Scan/Component.ts
+++ model/Xray/Scan/Component.ts
@@ -1,6 +1,9 @@
 import { IImpactPath } from '.';
 
 export interface IComponent {
+    package_name: string;
+    package_version: string;
+    package_type: string;
     fixed_versions: string[];
     impact_paths: IImpactPath[][];
 }
