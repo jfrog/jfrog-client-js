f4aa23159b02c41ca363de1453e1b6bff551a9db
Or Geva <orgab@jfrog.com>
2022-12-16 11:59:21 +0200

    Rename 'IResearch' to IExtendedInformation

---
+++ model/Xray/Scan/extendedInformation.ts
@@ -0,0 +1,15 @@
+import { Severity } from '../Severity';
+
+export interface IExtendedInformation {
+  short_description: string;
+  full_description: string;
+  remediation?: string;
+  jfrog_research_severity:Severity
+  jfrog_research_severity_reasons?: ISeverityReasons[];
+}
+
+export interface ISeverityReasons {
+  name: string;
+  description: string;
+  is_positive: string;
+}
