306b068857997ea1063d600e865a32de50f522b5
attiasas <assafattias93@gmail.com>
2022-12-18 17:50:46 +0200

    Support more fields to the scan/graph response

--- model/Xray/Scan/extendedInformation.ts
+++ model/Xray/Scan/extendedInformation.ts
@@ -4,12 +4,12 @@ export interface IExtendedInformation {
   short_description: string;
   full_description: string;
   remediation?: string;
-  jfrog_research_severity:Severity
+  jfrog_research_severity: Severity
   jfrog_research_severity_reasons?: ISeverityReasons[];
 }
 
 export interface ISeverityReasons {
   name: string;
   description: string;
-  is_positive: string;
+  is_positive: boolean;
 }
