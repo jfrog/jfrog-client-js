d0c8b1ae1b020beb2a3889ac114cdd9dec545f70
Assaf Attias <49212512+attiasas@users.noreply.github.com>
2022-12-19 16:47:25 +0200

    Support more fields to the scan/graph response (#65)

--- model/Xray/Scan/Violation.ts
+++ model/Xray/Scan/Violation.ts
@@ -2,4 +2,5 @@ import { IGraphLicense, IVulnerability } from '.';
 
 export interface IViolation extends IVulnerability, IGraphLicense {
     watch_name: string;
+    updated: string;
 }
--- model/Xray/Scan/extendedInformation.ts
+++ model/Xray/Scan/extendedInformation.ts
@@ -1,15 +1,15 @@
 import { Severity } from '../Severity';
 
 export interface IExtendedInformation {
-  short_description: string;
-  full_description: string;
-  remediation?: string;
-  jfrog_research_severity:Severity
-  jfrog_research_severity_reasons?: ISeverityReasons[];
+    short_description: string;
+    full_description: string;
+    remediation?: string;
+    jfrog_research_severity: Severity;
+    jfrog_research_severity_reasons?: ISeverityReasons[];
 }
 
 export interface ISeverityReasons {
-  name: string;
-  description: string;
-  is_positive: string;
+    name: string;
+    description: string;
+    is_positive: boolean;
 }
