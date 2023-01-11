d8ec1840b151b991011b99c2ac42efe11556c604
attiasas <assafattias93@gmail.com>
2023-01-11 12:25:19 +0200

    add ignore_url attribute to violation

--- model/Xray/Scan/Violation.ts
+++ model/Xray/Scan/Violation.ts
@@ -2,5 +2,6 @@ import { IGraphLicense, IVulnerability } from '.';
 
 export interface IViolation extends IVulnerability, IGraphLicense {
     watch_name: string;
+    ignore_url: string;
     updated: string;
 }
