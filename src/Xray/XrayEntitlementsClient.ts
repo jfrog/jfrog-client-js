7d06ace43444de8a52a0962823d20ef1b79406e5
attiasas <assafattias93@gmail.com>
2023-02-19 09:55:26 +0200

    rename endpoint variable

--- src/Xray/XrayEntitlementsClient.ts
+++ src/Xray/XrayEntitlementsClient.ts
@@ -2,14 +2,14 @@ import { HttpClient, IRequestParams } from '../HttpClient';
 import { IClientResponse, ILogger } from '../../model/';
 
 export class XrayEntitlementsClient {
-    private readonly entitlementsEndpoint: string = '/api/v1/entitlements/feature/';
+    private readonly entitlementsFeatureEndpoint: string = '/api/v1/entitlements/feature/';
 
     constructor(private readonly httpClient: HttpClient, private readonly logger: ILogger) {}
 
     public async feature(feature: string): Promise<boolean> {
         this.logger.debug("Sending entitlement request for feature '" + feature + "'...");
         const requestParams: IRequestParams = {
-            url: this.entitlementsEndpoint + feature,
+            url: this.entitlementsFeatureEndpoint + feature,
             method: 'GET',
             timeout: HttpClient.DEFAULT_TIMEOUT_IN_MILLISECONDS,
         };
