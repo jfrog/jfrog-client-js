5479cf04b635b387634e2c1d29c80eeebd3d2d47
Or Geva <orgab@jfrog.com>
2023-06-27 18:33:21 +0300

    Revert changes - Part 2

--- model/ClientConfig.ts
+++ model/ClientConfig.ts
@@ -16,4 +16,4 @@ export interface IClientConfig {
     retryDelay?: number;
 }
 
-export type RetryOnStatusCode = (error?: number) => boolean;
+export type RetryOnStatusCode = (statusCode?: number) => boolean;
