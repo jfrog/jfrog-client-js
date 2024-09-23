538a20ac841c2d4fb0b12004b11df2d2cdeeddaa
barv <barv@jfrog.com>
2024-09-23 18:20:13 +0300

    Jas config api for client-js

--- src/Xray/XrayJasConfigClient.ts
+++ src/Xray/XrayJasConfigClient.ts
@@ -9,34 +9,19 @@ export class XrayJasConfigClient {
      *
      * Sends 'GET /configuration/js requests to Xray and waits for 200 response'.
      * @returns the jas config
-     * @throws an exception if an unexpected response received from Xray or if checkCanceled threw an exception
+     * @throws an exception if an unexpected response received from Xray
      */
     async getJasConfig(): Promise<IJasConfig> {
         this.logger.debug(`Sending GET ${this.jasConfigurationEndpoint} request...`);
-        let receivedStatus: number | undefined;
         const requestParams: IRequestParams = {
             url: this.jasConfigurationEndpoint,
             method: 'GET',
             validateStatus: (status: number): boolean => {
-                receivedStatus = status;
                 return status === 200;
             },
         };
-        let message: string | undefined;
-        let response: IClientResponse | undefined;
-        try {
-            response = await this.httpClient.doAuthRequest(requestParams);
-        } catch (error) {
-            throw new Error(`Received unexpected response from Xray. ${String(error)}`);
-        }
-        this.logger.debug(`Received status '${receivedStatus}' from Xray.`);
-        if (receivedStatus === 200) {
-            return response?.data;
-        } else {
-            if (receivedStatus) {
-                throw new Error(`Received unexpected status '${receivedStatus}' from Xray: ${message}`);
-            }
-            throw new Error(`Received response from Xray: ${message}`);
-        }
+        return await (
+          await this.httpClient.doAuthRequest(requestParams)
+        ).data;
     }
 }
--- test/tests/Xray/XrayJasConfig.spec.ts
+++ test/tests/Xray/XrayJasConfig.spec.ts
@@ -2,12 +2,8 @@ import faker from 'faker';
 import nock from 'nock';
 import { JfrogClient } from '../../../src';
 import { TestUtils } from '../../TestUtils';
-
-let jfrogClient: JfrogClient;
-
-beforeAll(() => {
-    jfrogClient = new JfrogClient(TestUtils.getJfrogClientConfig());
-});
+import { IGraphRequestModel } from '../../../model';
+import { IJasConfig } from '../../../model/Xray/JasConfig/JasConfig';
 
 describe('Xray jas config tests', () => {
     test('Get jas config', async () => {
@@ -15,7 +11,24 @@ describe('Xray jas config tests', () => {
         const uri: string = `/xray/api/v1/configuration/jas`;
         const expectedResource: string = '{"enable_token_validation_scanning": true}';
         nock(PLATFORM_URL).get(uri).reply(200, expectedResource);
-        const res: any = await jfrogClient.xray().jasconfig().getJasConfig();
-        expect(res).toEqual(JSON.parse(expectedResource));
+        const client: JfrogClient = new JfrogClient({ platformUrl: PLATFORM_URL, logger: TestUtils.createTestLogger() });
+        const res: any = await client.xray().jasconfig().getJasConfig();
+        expect(res).toHaveProperty("enable_token_validation_scanning")
+        expect(res.enable_token_validation_scanning).toEqual(true)
+    });
+});
+
+describe('Xray jas config tests', () => {
+    test('Fail get jas config', async () => {
+        const PLATFORM_URL: string = faker.internet.url();
+        const uri: string = `/xray/api/v1/configuration/jas`;
+        nock(PLATFORM_URL).get(uri).reply(402, { message: 'error' }).persist();
+        const client: JfrogClient = new JfrogClient({ platformUrl: PLATFORM_URL, logger: TestUtils.createTestLogger() })
+        await expect(async () => {
+            await client
+              .xray()
+              .jasconfig()
+              .getJasConfig();
+        }).rejects.toThrow(`Request failed with status code 402`);
     });
 });
